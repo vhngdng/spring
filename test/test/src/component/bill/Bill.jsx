dd62731639880bab117277df02c58e52e5fc9518
dung <vhngdng@gmail.com>
2022-12-29 01:31:39 +0700

    fix

--- test/test/src/component/bill/Bill.jsx
+++ test/test/src/component/bill/Bill.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 
-function Bill({totalMoney}) {
-  console.log(totalMoney);
+function Bill({ totalMoney }) {
+	console.log(totalMoney);
 	return (
-		<div className="col-md-4">
-			<div className="bill">
-				<div className="border mb-2 p-3 fs-5 fw-normal d-flex justify-content-between align-items-center">
-					<span className="text-black-50">Tạm tính:</span>
-					<span className="text-primary" id="sub-total-money">
-						{totalMoney}
-					</span>
-				</div>
-				<div className="border mb-2 p-3 fs-5 fw-normal d-flex justify-content-between align-items-center">
-					<span className="text-black-50">VAT (10%):</span>
-					<span className="text-primary" id="vat-money">
-						{totalMoney/10}
-					</span>
-				</div>
-				<div className="border mb-2 p-3 fs-5 fw-normal d-flex justify-content-between align-items-center">
-					<span className="text-black-50">Thành tiền:</span>
-					<span className="text-primary" id="total-money">
-						{totalMoney + totalMoney/10}
-					</span>
-				</div>
+		<div className="bill">
+			<div className="border mb-2 p-3 fs-5 fw-normal d-flex justify-content-between align-items-center">
+				<span className="text-black-50">Tạm tính:</span>
+				<span className="text-primary" id="sub-total-money">
+					{totalMoney}
+				</span>
+			</div>
+			<div className="border mb-2 p-3 fs-5 fw-normal d-flex justify-content-between align-items-center">
+				<span className="text-black-50">VAT (10%):</span>
+				<span className="text-primary" id="vat-money">
+					{totalMoney / 10}
+				</span>
+			</div>
+			<div className="border mb-2 p-3 fs-5 fw-normal d-flex justify-content-between align-items-center">
+				<span className="text-black-50">Thành tiền:</span>
+				<span className="text-primary" id="total-money">
+					{totalMoney + totalMoney / 10}
+				</span>
 			</div>
 		</div>
 	);
